fb9a00d1ca98f51dcd69ae4f28814b782b0f3eea
Abbas Gheydi <abbas.gheydi@gmail.com>
2022-08-25 17:35:03 +0430

    Feat: Add rest api get method

--- pkgs/web/api.go
+++ pkgs/web/api.go
@@ -1,18 +1,52 @@
 package web
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func apiGetUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	userName := params["userName"]
-	user := userCode{UserName: userName}
+type JsonUSer struct {
+	UserName     string `json:"username"`
+	Status       string `json:"status"`
+	OtpCode      string `json:"otp_code,omitempty"`
+	ResponseCode int    `json:"-"`
+}
 
+func getUserNameParamFromurl(r *http.Request) userCode {
+	params := mux.Vars(r)
+	userName := params["username"]
+	user := userCode{UserName: userName}
+	return user
+
+}
+func makeJsonResponse(w http.ResponseWriter, juser JsonUSer, statusCode int) {
+	w.WriteHeader(statusCode)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(juser)
+}
+
+func newjsonUser(userName string, status string, otpCode string) JsonUSer {
+	return JsonUSer{UserName: userName, Status: status, OtpCode: otpCode}
+
+}
+
+func apiGetUser(w http.ResponseWriter, r *http.Request) {
+
+	respCode := http.StatusOK
+
+	user := getUserNameParamFromurl(r)
 	searchuser(&user)
-	fmt.Fprint(w, user.Result)
+
+	userInJson := newjsonUser(user.UserName, user.Result, user.Qr)
+	if user.Err != nil {
+		respCode = http.StatusInternalServerError
+	}
+
+	makeJsonResponse(w, userInJson, respCode)
+
+	//if user
+	//fmt.Fprint(w, user.Result)
 
 }
