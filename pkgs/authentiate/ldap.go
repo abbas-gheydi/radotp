51c6c1321cd3c4f3bceb3d5a986fa2923b2e82fd
Abbas Gheydi <abbas.gheydi@gmail.com>
2022-08-29 21:43:49 +0430

    Feat: add time switchToAnotherLdapServerInterval to changeLdapSegver and make it thread safe

--- pkgs/authentiate/ldap.go
+++ pkgs/authentiate/ldap.go
@@ -3,8 +3,16 @@ package authentiate
 import (
 	"log"
 	"strings"
+	"sync"
+	"time"
 
 	ldapAuth "github.com/korylprince/go-ad-auth/v3"
+)
+
+var (
+	ldapMutex                         sync.RWMutex
+	lastTimeLdapProviderChanged       time.Time
+	switchToAnotherLdapServerInterval = time.Second * 30
 )
 
 type LdapProvider struct {
@@ -13,27 +21,34 @@ type LdapProvider struct {
 	LdapServers []string
 }
 
-func (l LdapProvider) changeLapSegver() {
-	if len(l.LdapServers) > 1 {
+func (l LdapProvider) changeLdapSegver() {
+	if len(l.LdapServers) > 1 && time.Now().After(lastTimeLdapProviderChanged.Add(switchToAnotherLdapServerInterval)) {
+		ldapMutex.Lock()
 
 		for _, srv := range l.LdapServers {
 			if srv == l.LdapConfig.Server {
 				continue
 			}
+
 			l.LdapConfig.Server = srv
 			log.Println("change ldap server to ", srv)
+			lastTimeLdapProviderChanged = time.Now()
+
 			break
 
 		}
+		ldapMutex.Unlock()
 
 	}
 
 }
 
 func (l LdapProvider) IsUserAuthenticated(username string, password string) (authStat bool, groups []string) {
+	ldapMutex.RLock()
 	//log.Println("ldap server address", l.LdapConfig.Server)
 	authStat, _, groups, err := ldapAuth.AuthenticateExtended(l.LdapConfig, username, password, []string{"cn"}, l.Groups)
 	//log.Printf("status %v entry %v groups %v", authStat, entry, groups)
+	defer ldapMutex.RLocker().Unlock()
 
 	if err != nil {
 
@@ -46,7 +61,7 @@ func (l LdapProvider) IsUserAuthenticated(username string, password string) (aut
 		}
 		//try another ldap server
 		if strings.Contains(err.Error(), "Connection error") {
-			l.changeLapSegver()
+			go l.changeLdapSegver()
 		}
 
 		return
